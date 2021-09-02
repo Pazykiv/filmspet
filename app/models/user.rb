a75008045a2ba5a8935adc089581085aa918cd5d
Pazykiv <pazykiv@ukr.net>
2021-09-02 11:58:14 +0300

    RUB-11: Setup devise user, add role to user, use enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+
+  enum role: [:standard, :premium, :admin]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_initialize do
+    if self.new_record?
+      self.role ||= :standard
+    end
+  end
 end
