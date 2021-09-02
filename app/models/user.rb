abff179c61dd4c730298a6493624e9e73d93bd41
Pazykiv <pazykiv@ukr.net>
2021-09-02 15:17:50 +0300

    RUB-11: Setup devise user, change enum to hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  enum role: [:standard, :premium, :admin]
+  enum role: { customer: 0, admin: 1 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   after_initialize do
     if self.new_record?
-      self.role ||= :standard
+      self.role ||= :customer
     end
   end
 end
